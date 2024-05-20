9ddff99b8ccd7c5e7c640e70a4f9c6f8e8f0715a
Annappa Gowda <annappg2020@gmail.com>
2024-05-20 23:57:09 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,35 @@
+import "./Navbar.css";
 
-const LOGO_URL = "https://cdn.dribbble.com/userupload/5222793/file/original-46d3ef1abd8fba5f6b21c603cf01acd6.png?resize=752x"
+const LOGO_URL =
+  "https://cdn.dribbble.com/userupload/5222793/file/original-46d3ef1abd8fba5f6b21c603cf01acd6.png?resize=752x";
 const Navbar = () => {
   return (
     <div className="navigation">
-      <a href="">
-        <img src={LOGO_URL} alt="logo" />
+      <a href="/">
+        <img src={LOGO_URL} className="logo" alt="logo" />
       </a>
       <div className="navigation-menu">
         <ul>
-          <a href="#about">
-            <li>About</li>
-          </a>
-          <a href="#destinations">
-            <li>Destinations</li>
-          </a>
-          <a href="#newsletters">
-            <li>NewsLetters</li>
-          </a>
+          <li>
+            <a href="#home" className="nav-link nav-link-line">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="nav-link nav-link-line">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#destination" className="nav-link nav-link-line">
+              Destination
+            </a>
+          </li>
+          <li>
+            <a href="#newsletter" className="nav-link nav-link-line">
+              NewsLetter
+            </a>
+          </li>
         </ul>
       </div>
     </div>
---
+++ src/components/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+const LandingPage = () => {
+  return <div className="landinpage-container">
+    <div className="landingpage-content">
+        <h1>Exploag</h1>
+        <h3>Hdsadaadkaldlad& asdjada</h3>
+    </div>
+    
+  </div>;
+};
+
+export default LandingPage;
