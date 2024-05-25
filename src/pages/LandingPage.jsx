9460538a4718f1be7046ab80f4f92541fcf1cfdf
Annappa Gowda <annappg2020@gmail.com>
2024-05-25 22:15:53 +0530

    Landing Page Completed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
 import "./LandingPage.css";
 import videoSrc from "../../utils/video/bc_video.mp4";
 const LandingPage = () => {
   return (
     <div className="landinpage-container">
       <div className="landingpage-content">
-        <h1>Exploag</h1>
-        <h3>Hdsadaadkaldlad& asdjada</h3>
+        <h1>Explore Karnataka </h1>
+        <h3>Book Your Trip With The Trip Pole</h3>
+        <h4>Have Fun on your trip with exiting team</h4>
+        <Link to="/booknow">
+          <button className="lading-page-btn">Book Now</button>
+        </Link>
       </div>
       <div className="arrow bounce"></div>
       <video autoPlay loop muted id="myVideo">
